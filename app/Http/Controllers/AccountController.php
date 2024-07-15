65556c10239c571b06087010f7bba261e6af3c92
Nur Jannat <nurjannat200029@gmail.com>
2024-07-15 17:11:37 +0600

    User Registration

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AccountController extends Controller
+{
+    //Show user registration page
+    public function registration(){
+        return view('front.account.registration');
+    }
+
+    //Save user
+    public function processRegistration(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:4|same:confirm_password',
+            'confirm_password' => 'required',
+        ]);
+
+        if($validator->passes()){
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password =Hash::make($request->password);
+            $user-> save();
+
+            session()->flash('success','You Hava Register Successfully');
+
+            return response()->json([
+                'status' => true,
+                'errors' => []
+            ]);
+        }else{
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+    }
+
+    //Show user login page
+    public function login(){
+        return view('front.account.login');
+        
+    }
+}
