fb95cfe55c80486b7500eea311a4d9e9a282235e
Nur Jannat <nurjannat200029@gmail.com>
2024-07-15 19:16:41 +0600

    User Authentication

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -48,6 +49,36 @@ class AccountController extends Controller
     //Show user login page
     public function login(){
         return view('front.account.login');
-        
+    }
+
+    //user login authenticate
+    public function authenticate(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if($validator->passes()){
+            if(Auth::attempt(['email'=>$request->email, 'password'=>$request->password])){
+                return redirect()->route('account.profile');
+            }else{
+                return redirect()->route('account.login')
+                ->with('error','Either email/password is incorrect');
+            }
+        }else{
+            return redirect()->route('account.login')
+            ->withErrors($validator)
+            ->withInput($request->only('email'));
+        }
+    }
+
+    //Show user profile page
+    public function profile(){
+        return view('front.account.profile');
+    }
+
+    //Show user profile page
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('account.login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'index'])->name('home');
 
-Route::get('/account/register',[AccountController::class,'registration'])->name('account.registration');
-Route::post('/account/process-register',[AccountController::class,'processRegistration'])->name('account.processRegistration');
 
-Route::get('/account/login',[AccountController::class,'login'])->name('account.login');
+
+
+
+Route::group(['account'],function(){
+    //Guest
+    Route::group(['middleware' => 'guest'],function(){
+        Route::get('/account/register',[AccountController::class,'registration'])->name('account.registration');
+        Route::post('/account/process-register',[AccountController::class,'processRegistration'])->name('account.processRegistration');
+        Route::get('/account/login',[AccountController::class,'login'])->name('account.login');
+        Route::post('/account/authenticate',[AccountController::class,'authenticate'])->name('account.authenticate');
+    });
+    //Authenticate
+    Route::group(['middleware' => 'auth'],function(){
+        Route::get('/account/profile',[AccountController::class,'profile'])->name('account.profile');
+        Route::get('/account/logout',[AccountController::class,'logout'])->name('account.logout');
+    });
+});
